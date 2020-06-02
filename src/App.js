582a2b7fe8e558536cc5c0cc18e3f966bd017263
Shpakova Alena <shpakovadev@gmail.com>
2020-06-02 17:12:53 +0300

    get data in list and style card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import './App.scss'
-import ListItem from './components/listItem'
+import List from './components/list'
 import Details from './components/details'
 import Filter from './components/filter'
 
 const App = () => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
+
   const handleShow = () => setShow(true)
 
   return (
     <div className="app">
       <Filter />
-      <ListItem handleShow={handleShow} />
+      <List handleShow={handleShow} />
       <Details handleClose={handleClose} show={show} />
     </div>
   )
