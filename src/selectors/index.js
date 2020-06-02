b5bf8a0e9e0f3694fe2168499129aec0f12cfd6a
Shpakova Alena <shpakovadev@gmail.com>
2020-06-02 20:27:13 +0300

    fix selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,5 +3,10 @@ export const getFilteredValue = state => {
     return state.list
   }
 
-  return state.list.filter(e => e.title.indexOf(state.filter) >= 0)
+  return state.list.filter(
+    e =>
+      e.title.indexOf(state.filter) >= 0 ||
+      (e.description && e.description.indexOf(state.filter) >= 0) ||
+      e.type.indexOf(state.filter) >= 0
+  )
 }
